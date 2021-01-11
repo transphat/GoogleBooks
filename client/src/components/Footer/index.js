f4784f680fee9591429e072887ea1e7557c60054
Dung Tran <trankhadung@gmail.com>
2021-01-11 00:11:44 -0500

    first commit

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+	<footer className = "page-footer">
+		<div className = "container-fluid text-center">
+			<p>Google Books - Dung Tran</p>
+		</div>	
+	</footer>
+);
+
+export default Footer;
